925208de515892c0fe3718e6f0ab3a90dc0f245f
syed jaid <96988354+syed-jaid@users.noreply.github.com>
2025-08-13 19:46:54 +0600

    card centered

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const notificationDiv = document.querySelector(".confetti-image");
-  const elementWithText = document.querySelector(".element-with-text");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const notificationDiv = document.querySelector(".confetti-image");
+//   const elementWithText = document.querySelector(".element-with-text");
 
-  notificationDiv.addEventListener("animationend", function () {
-    elementWithText.classList.add("element-with-text-animation");
-  });
-});
+//   notificationDiv.addEventListener("animationend", function () {
+//     elementWithText.classList.add("element-with-text-animation");
+//   });
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const elementWithText = document.querySelector(".element-with-text");
-  const phoneScreen = document.querySelector(".phone-screen");
-  const phoneFrame = document.querySelector(".phone-frame-1");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const elementWithText = document.querySelector(".element-with-text");
+//   const phoneScreen = document.querySelector(".phone-screen");
+//   const phoneFrame = document.querySelector(".phone-frame-1");
 
-  elementWithText.addEventListener("animationend", function () {
-    phoneScreen.style.display = "none";
-    phoneFrame.style.opacity = 0;
-  });
-});
+//   elementWithText.addEventListener("animationend", function () {
+//     phoneScreen.style.display = "none";
+//     phoneFrame.style.opacity = 0;
+//   });
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const phoneScreen = document.querySelector(".Phone-main-div");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const phoneScreen = document.querySelector(".Phone-main-div");
 
-  phoneScreen.style.display = "flex";
-});
+//   phoneScreen.style.display = "flex";
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const elementWithText = document.querySelector(".element-with-text");
-  const phoneMainDiv = document.querySelector(".Phone-main-div");
-  const nextDiv = document.querySelector(".card-main-div");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const elementWithText = document.querySelector(".element-with-text");
+//   const phoneMainDiv = document.querySelector(".Phone-main-div");
+//   const nextDiv = document.querySelector(".card-main-div");
 
-  elementWithText.addEventListener("animationend", () => {
-    setTimeout(() => {
-      phoneMainDiv.style.display = "none";
-      nextDiv.style.display = "flex";
-    }, 2000);
-    s;
-  });
-});
+//   elementWithText.addEventListener("animationend", () => {
+//     setTimeout(() => {
+//       phoneMainDiv.style.display = "none";
+//       nextDiv.style.display = "flex";
+//     }, 2000);
+//     s;
+//   });
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const mainInnerCard = document.querySelector(".main-inner-card");
-  const MainCardDiv = document.querySelector(".card-main-div");
-  const nextDiv = document.querySelector(".phone-2-main-div");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const mainInnerCard = document.querySelector(".main-inner-card");
+//   const MainCardDiv = document.querySelector(".card-main-div");
+//   const nextDiv = document.querySelector(".phone-2-main-div");
 
-  mainInnerCard.addEventListener("animationend", () => {
-    setTimeout(() => {
-      MainCardDiv.style.display = "none";
-      nextDiv.style.display = "flex";
-    }, 5000);
-    s;
-  });
-});
+//   mainInnerCard.addEventListener("animationend", () => {
+//     setTimeout(() => {
+//       MainCardDiv.style.display = "none";
+//       nextDiv.style.display = "flex";
+//     }, 5000);
+//     s;
+//   });
+// });
 
-document.querySelector(".replay-btn").addEventListener("click", () => {
-  window.location.reload();
-});
+// document.querySelector(".replay-btn").addEventListener("click", () => {
+//   window.location.reload();
+// });
